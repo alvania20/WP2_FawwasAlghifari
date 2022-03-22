29860a940af57da247445d55397010b9524a6dfe
Alvania20 <fawwasalghifari20@gmail.com>
2022-03-22 09:21:19 +0700

    merubah beberapa file

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,8 +4,8 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo "Nama saya Khairul Azhar
-            Saya tinggal di daerah Bintaro
-            Olahraga yang saya sukai adalah berenang";
+        echo "Nama saya Fawwas Alghifari
+            Saya tinggal di daerah Ciputat
+            Olahraga yang saya sukai adalah ngk tau";
     }
 }
